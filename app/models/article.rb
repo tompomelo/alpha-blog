50f6f487e78f277417433feb8930b82ee48bad01
empty herring <tom.still@live.co.uk>
2017-04-04 14:09:00 +0000

    Fixed line breaks and text wrapping for title and description

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-    validates :description, presence: true, length: { minimum: 10, maximum: 9999}
+    validates :description, presence: true, length: { minimum: 10, maximum: 40000}
 end
 
 def breaking_word_wrap(text, *args)
