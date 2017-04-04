2317c3a80afdc755439e89d5ede295beb611a391
empty herring <tom.still@live.co.uk>
2017-04-04 13:06:00 +0000

    Fixed text overflow on articles listing page and article show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
 class Article < ActiveRecord::Base
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-    validates :description, presence: true, length: { minimum: 10, maximum: 300}
+    validates :description, presence: true, length: { minimum: 10, maximum: 9999}
+end
+
+def breaking_word_wrap(text, *args)
+   options = args.extract_options!
+   unless args.blank?
+     options[:line_width] = args[0] || 80
+   end
+   options.reverse_merge!(:line_width => 80)
+   text = text.split(" ").collect do |word|
+     word.length > options[:line_width] ? word.gsub(/(.{1,#{options[:line_width]}})/, "\\1 ") : word
+   end * " "
+   text.split("\n").collect do |line|
+     line.length > options[:line_width] ? line.gsub(/(.{1,#{options[:line_width]}})(\s+|$)/, "\\1\n").strip : line
+   end * "\n"
 end
