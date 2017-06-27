bfd7985063d6e98b928545c1a23478c82a26f0a2
empty herring <tom.still@live.co.uk>
2017-06-27 13:08:10 +0000

    Fixed routing error, fixed paginate array error

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
+  
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
+  resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :categories, except: [:destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  
-  root "pages#home"
-  
-  get "about", to: "pages#about"
-  
-  resources :articles
-  
-  get "signup", to: "users#new"
-  resources :users, except: [:new]
-  
-  get "login", to: "sessions#new"
-  post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
-  
-  resources :categories, except: [:destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
